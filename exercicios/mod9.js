00baebe103f9835d28f1ad334b969829cc0780ff
Bruna Nisa <bnisa.dev@gmail.com>
2022-12-22 15:54:32 -0300

    feat: exercicio modulo 9

--- exercicios/mod9.js
+++ exercicios/mod9.js
@@ -2,31 +2,19 @@ $(document).ready(function(){
     $("form").on("submit", function(e){
         e.preventDefault();
 
-        const nomeDaTarefa = $("#input-nome-tarefa").val();
-        const novaTarefa = $(`<li id="${Math.random().toFixed(3) *1000}">${nomeDaTarefa}</li>`);
-        $("#lista-de-tarefas").append(novaTarefa);
-        console.log(novaTarefa);
-        $("#input-nome-tarefa").val("");
+        adicionaTarefa();
     })
 
-    /*
-    $("#lista-de-tarefas li").click(function(this){
-        
-        console.log($(this).css("extDecorationLineThrough"));
-        $("#lista-de-tarefas li").last().addClass( "selected highlight" );
-        console.log( $("#lista-de-tarefas li").ready(function(e){
-
-        })
+    $("ul").on("click","li",function(){
+        $(this).css("text-decoration", "line-through");
     })
-    */
-
-    $("#lista-de-tarefas li").click(function(){
-        console.log("classe");
-        let classe = this.className;
-        let ident = $(this).attr("id");
-        console.log("classe");
-        console.log(ident);
-    })
-
-
 })
+
+function adicionaTarefa(){
+    const nomeDaTarefa = $("#input-nome-tarefa").val();
+    const novaTarefa = $(`<li></li>`);
+    $(novaTarefa).append(nomeDaTarefa);
+    $("#lista-de-tarefas").append(novaTarefa);
+    
+    $("#input-nome-tarefa").val("");
+}
